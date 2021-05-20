698c0bba7d982da2ed9d13ae9ac3461390c62868
Mario Alberto Cabrera Alfaro <maca.15c@gmail.com>
2021-05-20 10:13:17 -0500

    initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        \App\Models\User::create([
+            'name' => 'Usuario Root',
+            'username' => 'root',
+            'password' => Hash::make('ssa.audit'),
+            'email' => 'root@localhost',
+            'is_superuser' => 1,
+            'avatar' => '/assets/avatars/50-king.svg'
+        ]);
+
+        $this->call(PermissionsTableSeeder::class);
+    }
+}
